fe251783d4de429fd62d069f5044fce43deced8f
Ram Maheshwari <ram991011@gmail.com>
2021-06-26 15:40:22 +0530

    added form submit which prints data in console

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ---
 const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
 const smallMenu = document.querySelector('.header__sm-menu')
 
@@ -7,4 +8,26 @@ hamMenuBtn.addEventListener('click', () => {
   } else {
     smallMenu.classList.add('header__sm-menu--active')
   }
+})
+
+// ---
+const formData = {
+  name: '',
+  email: '',
+  number: '',
+  message: '',
+}
+
+const contactFormInputs = document.querySelectorAll('.contact__form-input')
+const contactForm = document.querySelector('.contact__form')
+
+contactFormInputs.forEach((input) => {
+  input.addEventListener('input', (e) => {
+    formData[input.attributes['name'].value] = e.target.value
+  })
+})
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log(formData)
 })
